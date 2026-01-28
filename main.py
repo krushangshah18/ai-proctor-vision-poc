18ec0a64180c12f971111ba12921abe344a7274b
krushangshah18 <krushang.shah@drcsystems.com>
2026-01-28 12:55:38 +0530

    feat: add basic webcam capture and display loop

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import cv2
+
+
 def main():
     print("Hello from ai-proctor-vision-poc!")
+    
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise RuntimeError("Could Not open WebCam")
+    
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        cv2.imshow("AI Proctoring Vision POC", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break 
+    
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
